9c3af99c30c36406b7252491fd75c47e3a6159ba
rafaelarm2 <42384623+rafaelarm2@users.noreply.github.com>
2018-11-26 23:54:30 -0200

    Update Produto.cpp

--- Produto.cpp
+++ Produto.cpp
@@ -1,3 +1,4 @@
+
 #include "Produto.h"
 
 Produto::Produto(std::string nome, double preco, std::string marca) {
@@ -5,6 +6,14 @@ Produto::Produto(std::string nome, double preco, std::string marca) {
 	this->nome = nome;
 	this->preco = preco;
 	this->marca = marca;
+}
+
+Produto::Produto(int id, std::string nome, double preco, std::string marca) {
+	// TODO Auto-generated constructor stub
+	this->nome = nome;
+	this->preco = preco;
+	this->marca = marca;
+	this->id = id;
 }
 
 Produto::Produto(){ // @suppress("Class members should be properly initialized")
@@ -17,4 +26,12 @@ Produto::~Produto() {
 
 double Produto::getPreco() {
 	return this->preco;
+}
+
+std::string Produto::getMarca() {
+	return this->marca;
+}
+
+std::string Produto::getNome() {
+	return this->nome;
 }

eab1cea402c204f69bca021bc624b5bd3c96d7f2
rafaelarm2 <42384623+rafaelarm2@users.noreply.github.com>
2018-11-26 23:48:08 -0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,71 +1,173 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 25 de nov de 2018
+ */
+
 #include "Cliente.h"
-#include "Cidade.h"
 #include "Endereco.h"
 #include "Produto.h"
+#include "ControladorProduto.h"
 #include "Venda.h"
 #include "VendaEntrega.h"
 #include "Item.h"
 #include "Funcionario.h"
 #include "Pessoa.h"
 #include <string>
+#include <string.h>
 #include <list>
 #include <iostream>
+#include <stdio.h>
+#include <sqlite3.h>
+#include <stdlib.h>
+#include <curses.h>
+
+#define MAX 20
 
 using namespace std;
 
+void listarmenu() {
+	std::cout << "Menu"<< endl;
+	std::cout << "1. Venda" << endl;
+	std::cout << "2. CRUD Produto" << endl;
+	std::cout << "3. CRUD Cliente" << endl;
+	std::cout << "4. CRUD Funcionário\n" << endl;
+}
+
+void venda() {
+
+}
+
+void crud_produto() {
+		std::cout << "CRUD PRODUTO"<< endl;
+		std::cout << "1. Inserir" << endl;
+		std::cout << "2. Listar" << endl;
+		std::cout << "3. Buscar" << endl;
+		std::cout << "4. Remover" << endl;
+		std::cout << "5. Alterar" << endl;
+		std::cout << "6. Voltar" << endl;
+
+		int item_menu;
+		std::cout << "Escolha uma seção: ";
+		scanf("%d", &item_menu);
+
+
+		fflush(stdin);
+
+		ControladorProduto *cp = new ControladorProduto();
+		switch (item_menu) {
+		case 1: {
+			std::cout << "Nome: " << endl;
+			std::string nome;
+			std::cin.ignore();
+			std::getline(std::cin, nome);
+			std::cout << nome << endl;
+
+			std::cout << "Marca: " << endl;
+			std::string marca;
+			std::getline(std::cin, marca);
+			std::cout << marca << endl;
+
+			std::cout << "Preco: " << endl;
+			std::string preco;
+			std::getline(std::cin, preco);
+			std::cout << preco << endl;
+
+			Produto *p = new Produto(nome, atof(preco.c_str()), marca);
+
+			cp->inserirProduto(*p);
+
+		}break;
+
+		case 2: {
+			cp->listarProduto();
+
+		}break;
+
+		case 3: {
+			int id;
+			std::cout << "Insira um id para busca: ";
+			scanf("%d", &id);
+			cp->buscarProduto(id);
+		}break;
+
+		case 4: {
+			cp->listarProduto();
+			int id;
+			std::cout << "\nInsira um id para remover: ";
+			scanf("%d", &id);
+			cp->removerProduto(id);
+		} break;
+		case 5: {
+			cp->listarProduto();
+			int id;
+			std::cout << "\nInsira um id para alterar: ";
+			scanf("%d", &id);
+
+			std::cout << "Novo nome: " << endl;
+			std::string nome;
+			std::cin.ignore();
+			std::getline(std::cin, nome);
+			std::cout << nome << endl;
+
+			std::cout << "Nova marca: " << endl;
+			std::string marca;
+			std::getline(std::cin, marca);
+			std::cout << marca << endl;
+
+			std::cout << "Novo preco: " << endl;
+			std::string preco;
+			std::getline(std::cin, preco);
+			std::cout << preco << endl;
+
+			Produto *p = new Produto(nome, atof(preco.c_str()), marca);
+
+			cp->alterarProduto(id, *p);
+		}break;
+		case 6: {
+			printf("\n\n\n");
+			return;
+		}break;
+		}
+}
+
 int main() {
-	Cidade *cid = new Cidade();
+	while(1){
+		int item_menu;
+		listarmenu();
 
-	std::cout << cid->getNome() << endl;
+		std::cout << "Selecionar função: ";
+		scanf("%d", &item_menu);
+		switch (item_menu) {
+		case 1: {
+			// VENDA
 
-	Endereco *end = new Endereco(Logradouro::Rua, std::string("Guarapari"), std::string("Giovanini"), std::string("35170102"), 471,std::string(""), *cid);
+			printf("\n\n\n");
 
-	std::cout << end->getCidade().getNome() << endl;
+			venda();
+		}break;
+		case 2: {
+			//CRUD DE PRODUTO
+			printf("\n\n\n");
+			crud_produto();
+		}break;
+		case 3: {
 
-	Cliente *cli = new Cliente(std::string("Rafaela"),std::string("11550565664"), std::string("MG17033716"), std::string("moreiramrafaela@gmail.com"), *end, std::string("31988959190"), std::string("19/11/2018"), TipoCliente::OURO);
+		}break;
+		case 4: {
 
-	std::cout << cli->getEndereco().getCidade().getNome() << endl;
+		}break;
+		default: {
 
-	Produto *prod1 = new Produto(std::string("Condicionador"), 39.90, std::string("Viverdí"));
-	Produto *prod2 = new Produto(std::string("Shampoo"), 36.90, std::string("Viverdí"));
-	Produto *prod3 = new Produto(std::string("Máscara"), 42.00, std::string("Viverdí"));
+		}
+		}
 
-	std::cout << prod1->getPreco() << endl;
-	std::cout << prod2->getPreco() << endl;
-	std::cout << prod3->getPreco() << endl;
-
-
-	Item *it1 = new Item(*prod1, 3);
-	Item *it2 = new Item(*prod2, 2);
-	Item *it3 = new Item(*prod3, 1);
-
-	std::cout << it1->getVltotal() << endl;
-	std::cout << it2->getVltotal() << endl;
-	std::cout << it3->getVltotal() << endl;
-
-	std::list<Item> itens;
-	itens.push_back(*it1);
-	itens.push_back(*it2);
-	itens.push_back(*it3);
-
-	std::cout << "           " << endl;
+	}
+	////////////////////// CRUD DE PRODUTO ///////////////////////////////
 
 
-	Funcionario *func = new Funcionario();
-
-	Venda *v = new Venda(std::string("19/11/2018"), itens, *cli, *func, 0);
-
-	std::cout << v->getVlfinal() << endl;
-
-	VendaEntrega *ve = new VendaEntrega(std::string("19/11/2018"), itens, *cli, *func, 0, *end, std::string("29/11/2018"), 20.50, std::string("Embalagem A"));
-
-	std::cout << ve->getVlfinalcomfrete() << endl;
-
-	v->tenhofrete();
-	ve->tenhofrete();
 
 	return 0;
 }
-
 
 

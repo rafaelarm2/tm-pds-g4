b6b982363538f3659f721eae955eaf0fe6bcd48d
rafaelarm2 <42384623+rafaelarm2@users.noreply.github.com>
2018-11-26 23:48:47 -0200

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,24 +1,31 @@
-#include "Cliente.h"
+#ifndef CLIENTE_H_
+#define CLIENTE_H_
+
 #include "Pessoa.h"
 #include <string>
 
-Cliente::Cliente(std::string nome, std::string cpf, std::string rg, std::string email, Endereco endereco,
-		std::string tel, std::string dtcadastro, TipoCliente tipo): Pessoa(nome, cpf, rg, email, endereco, tel) {
-	this->dtcadastro = dtcadastro;
-	this->tipo = tipo;
+using namespace std;
 
-}
+enum TipoCliente {
+	PRATA, BRONZE, OURO, DIAMANTE
+};
 
-Cliente::Cliente(): Pessoa() { // @suppress("Class members should be properly initialized")
+class Cliente: public Pessoa {
+protected:
+	static int id;
+	std::string dtcadastro;
+	TipoCliente tipo;
+public:
+	Cliente(std::string nome, std::string cpf, std::string email, Endereco endereco, std::string tel,
+			std::string dtcadastro, TipoCliente tipo);
+	Cliente();
+	virtual ~Cliente();
+	int gerarId();
 
-}
+	Endereco getEndereco();
 
-Cliente::~Cliente() {
-	// TODO Auto-generated destructor stub
-}
 
-Endereco Cliente::getEndereco() {
-	return this->endereco;
-}
+};
 
+#endif /* CLIENTE_H_ */
 

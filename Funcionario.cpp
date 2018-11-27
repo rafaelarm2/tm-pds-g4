6297fdb0c21f378be2f4fbc624371205394fb805
rafaelarm2 <42384623+rafaelarm2@users.noreply.github.com>
2018-11-26 23:51:21 -0200

    Update Funcionario.cpp

--- Funcionario.cpp
+++ Funcionario.cpp
@@ -5,11 +5,9 @@
 
 using namespace std;
 
-Funcionario::Funcionario(std::string nome, std::string cpf, std::string rg, std::string email, Endereco endereco,
-		std::string tel, std::string usuario, std::string senha, std::string dtadmissao, std::string dtdemissao):
-		Pessoa(nome, cpf, rg, email, endereco, tel) {
-	this->usuario = usuario;
-	this->senha = senha;
+Funcionario::Funcionario(std::string nome, std::string cpf, std::string email, Endereco endereco,
+		std::string tel, std::string dtadmissao, std::string dtdemissao):
+		Pessoa(nome, cpf, email, endereco, tel) {
 	this->dtadmissao = dtadmissao;
 	this->dtdemissao = dtdemissao;
 
@@ -22,4 +20,3 @@ Funcionario::Funcionario(): Pessoa() {
 Funcionario::~Funcionario() {
 	// TODO Auto-generated destructor stub
 }
-

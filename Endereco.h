bf4312a50e47071c65ba6b7476986b51c01e1ddd
Danilo <danilosiqueira99@gmail.com>
2018-11-27 10:51:25 -0200

    endereço gets/sets

--- Endereco.h
+++ Endereco.h
@@ -21,7 +21,23 @@ public:
 	Endereco();
 	virtual ~Endereco();
 
+//gets
+	int getID();
+	std::string getLogradouro();
+	std::string getBairro();
+	std::string getCep();
+	int getNum();
+	std::string getComp();
 	std::string getCidade();
+
+//sets
+	void setID(int id_);
+	void setLogradouro(std::string logradouro_);
+	void setBairro(std::string bairro_);
+	void setCep(std::string cep_);
+	void setNum(int num_);
+	void setComp(std::string comp_);
+	void setCidade(std::string cidade_s);
 };
 
 #endif /* ENDERECO_H_ */

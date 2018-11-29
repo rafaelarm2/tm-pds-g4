358c53ee708f24fdf2499b8e965b1cf0b8302f78
andrefiuza42 <42918548+andrefiuza42@users.noreply.github.com>
2018-11-29 00:22:33 -0200

    Update excecoes.cpp

--- excecoes.cpp
+++ excecoes.cpp
@@ -2,3 +2,8 @@
 
 const char* SelecaoMenuInvalida::what() const noexcept{
     return "Opcao inserida invalida, por favor digite outro numero";
+}
+
+const char* InsercaoDeDadosIncorreta::what() const noexcept{
+        return "Argumentos invalidos";
+}

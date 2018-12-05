21073989e134b1e82c481ee8ee430dd74903e52c
andrefiuza42 <42918548+andrefiuza42@users.noreply.github.com>
2018-12-04 22:42:23 -0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,6 +26,62 @@
 
 using namespace std;
 
+bool checa_data(std::string str){
+    std::string::size_type sz;
+    
+    std::string dia;
+    dia[0] = str[0];
+    dia[1] = str[1];
+    
+    try {
+		std::stod (dia, &sz);
+	}
+	catch (const std::invalid_argument& ia){
+		std::cout << "Argumento invalido, por favor insira outro valor. " << std::endl;
+		return 0;
+	}
+    int day = std::stod (dia, &sz);
+    
+    
+    std::string mes;
+    mes[0] = str[3];
+    mes[1] = str[4];
+    
+    try {
+		std::stod (mes, &sz);
+	}
+	catch (const std::invalid_argument& ia){
+		std::cout << "Argumento invalido, por favor insira outro valor. " << std::endl;
+		return 0;
+	}
+    int month = std::stod (mes, &sz);
+    
+    
+    std::string ano;
+    ano[0] = str[6];
+    ano[1] = str[7];
+    ano[2] = str[8];
+    ano[3] = str[9];
+    
+    try {
+		std::stod (ano, &sz);
+	}
+	catch (const std::invalid_argument& ia){
+		std::cout << "Argumento invalido, por favor insira outro valor. " << std::endl;
+		return 0;
+	}
+    int year = std::stod (ano, &sz);
+    
+    if ( (str[5] == '/') && (str[2] == '/') ){
+        std::cout << "Formato de data valido, fique atento para as excecoes" << std::endl;
+        return true;
+    }
+    else{ 
+        std::cout << "Formato de data invalido, por favor insira outro valor!" << std::endl;
+        return false;
+    }
+}
+
 bool verifica_dados_endereco(std::string info){
     if((info == "") || (info == " ")){
 	    std::cout << "Invalido. Insira o argumento corretamente." << std::endl;

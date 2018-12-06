94cdd804fa506fcb0e2a091dd602e8392c89ea95
andrefiuza42 <42918548+andrefiuza42@users.noreply.github.com>
2018-12-05 23:05:45 -0200

    Update Pessoa.cpp

--- Pessoa.cpp
+++ Pessoa.cpp
@@ -4,10 +4,61 @@
 
 using namespace std;
 
+bool validaCPF(const char * cpf){
+    unsigned lenght = 0;
+    int ver1 = 0, ver2 = 0;
+    char aux;
+
+    /* contando o tamanho de cpf e verificando se o o tamanho do mesmo é 11
+     */
+    while (cpf[lenght]) {
+        lenght++;
+    }
+
+    if (lenght != 11)
+        return false;
+
+    /* Calculando o primeiro dígito verificador. Cada dígito tem um peso co-
+     * meçando de 1 até 9. Para calculá-lo deve-se somar cada dígito, multi-
+     * plicando-se por seu peso e ao final realizar a operação de módulo por
+     * 11
+     */
+    for (int j=0; j<9; j++){
+        aux = cpf[j];
+        ver1 += atoi(&aux)*(j+1);
+    }
+
+    ver1 %= 11;
+
+    /* Para o segundo dígito verificador calcula-se a partir do segundo dí-
+     * gito até o 10º ( dígito verificador 1 ) atribuindo-se o peso de 1  a
+     * 9, e no final realizar a operação Módulo por 11
+     */
+    for (int j=0; j<9; j++){
+        aux = cpf[j];
+        ver2 += atoi(&aux)*j;
+    }
+
+    ver2 += ver1*9;
+    ver2 %= 11;
+
+    /* compara-se agora ver1 e ver2 com os dígitos do cpf */
+    aux = cpf[lenght-2];
+    if (atoi(&aux) == ver1){
+        aux = cpf[lenght-1];
+        if ( atoi(&aux) == ver2)
+            return true;
+    }
+    return false;
+}
+
 Pessoa::Pessoa(std::string nome, std::string cpf, std::string email, Endereco endereco,
 		std::string tel) {
 	this->nome = nome;
-	this->cpf = cpf;
+	if(validaCpf(cpf) == 1)
+		this->cpf = cpf;
+	else 
+		throw InsercaoDeDadosInvalida();
 	this->email = email;
 	this->endereco = endereco;
 	this->tel = tel;
